5267bbef605573dd415cd5697db550087a88896e
Vinaya Sheni <vinayakumar@gmail.com>
2018-11-23 16:20:58 -0600

    updated files

--- aboutpets.js
+++ aboutpets.js
@@ -46,3 +46,60 @@ $(document).ready(function(){
 		});
    
 });
+
+function setReadOnly(canRead){
+    // $("#email1").prop("readonly", canRead);
+    $("#name").prop("readonly", canRead);
+    $("#age").prop("readonly", canRead);
+    $("#type").prop("disabled", canRead);
+    $("#breed").prop("readonly", canRead);
+    $("#size").prop("readonly", canRead);
+	  $("#petdescription").prop("readonly", canRead);
+  }
+  
+  
+ function saveChangesFunction(){
+  var name = $("#name").val();
+  var age = $("#age").val();
+  var type = $("#type").val();
+  var breed = $("#breed").val();
+  var size = $("#size").val();
+  var petdescription = $("#petdescription").val();
+ 
+  sessionStorage.setItem("name", name);
+  sessionStorage.setItem("age", age);
+  sessionStorage.setItem("type", type);
+  sessionStorage.setItem("breed", breed);
+  sessionStorage.setItem("size", size);
+   sessionStorage.setItem("petdescription", size);
+  $("#saveChangesButton").hide();
+  sessionStorage.setItem("editProfile", "false");
+  $("#editButton").show();
+ // $("#saveChangesSuccess").show();
+ // $("#successRegisterAlert").hide();
+  setReadOnly(true);
+  setInputValues();
+}
+function setInputValues(){
+  
+    var name = sessionStorage.getItem("name");
+    var age = sessionStorage.getItem("age");
+    var type = sessionStorage.getItem("type");
+    var breed = sessionStorage.getItem("breed");
+    var size = sessionStorage.getItem("size");
+    var petdescription = sessionStorage.getItem("petdescription");
+ 
+  
+    $("#name").val(name);
+    $("#age").val(age);
+    $("#type").val(tyoe);
+    $("#brees").val(breed);
+    $("#petdescription").val(petdescription);
+    setReadOnly(true);
+  }
+  
+  function editButtonFunction(){
+    $("#editButton").hide();
+    $("#saveChangesButton").show();
+    setReadOnly(false);
+  }
